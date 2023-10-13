cf8ffbca4a7b649372d97c903b51494d0c29ad84
Rüdiger Kessel <ruediger.kessel@gmail.com>
2023-10-13 22:55:31 +0200

    fix: phpcbf

--- remote.php
+++ remote.php
@@ -1,15 +1,17 @@
 <?php
-     
+
 //use dokuwiki\Extension\AdminPlugin;
 use dokuwiki\Remote\RemoteException;
-     
-class remote_plugin_asciidocjs extends DokuWiki_Remote_Plugin {
+
+class remote_plugin_asciidocjs extends DokuWiki_Remote_Plugin
+{
     /**
      * Returns details about the remote plugin methods
      *
      * @return array Information about all provided methods. {@see dokuwiki\Remote\RemoteAPI}
      */
-    public function _getMethods() {
+    public function _getMethods()
+    {
         return [
             'getMediaToken' => [
                 'args' => ['string','int','int'],
@@ -21,13 +23,13 @@ class remote_plugin_asciidocjs extends DokuWiki_Remote_Plugin {
             ]
         ];
     }
-     
-    public function getMediaToken($id, $w, $h) {
+
+    public function getMediaToken($id, $w, $h)
+    {
         return media_get_token($id, $w, $h);
     }
-    public function test() {
+    public function test()
+    {
         return 'Test';
     }
 }
-
-
